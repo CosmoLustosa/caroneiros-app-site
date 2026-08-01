5457bd5e97c19cbfa2c70642212c33198ea8a796
Cosmo Lustosa <cosmolustosa2@gmail.com>
2026-08-01 09:29:21 -0300

    Adição do Painel

--- firebase-messaging-sw.js
+++
@@ -1,5 +0,0 @@
-// Script necessário apenas para o Firebase Cloud Messaging funcionar na Web.
-// Para habilitar notificações push em background na web, adicione as credenciais do Firebase aqui no futuro.
-self.addEventListener('push', function(event) {
-  console.log('[ServiceWorker] Push Recebido.');
-});
